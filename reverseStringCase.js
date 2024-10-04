a5b52c49cae37e4c38b93f98767df00619c59f01
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2024-10-04 09:44:41 +0100

    string from number fix

---
+++ reverseStringCase.js
@@ -0,0 +1,34 @@
+function reverseStringCase(text) {
+  let reversedString = [];
+
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] == text[i].toUpperCase()) {
+      reversedString.push(text[i].toLowerCase());
+    } else {
+      reversedString.push(text[i].toUpperCase());
+    }
+  }
+
+  console.log(reversedString.join(""));
+  return reversedString.join("");
+}
+
+function reverseStringCases(text) {
+  const reversedString = text
+    .split("")
+    .map((item) =>
+      item === item.toUpperCase() ? item.toLowerCase() : item.toUpperCase()
+    )
+    .join("");
+
+  console.log(reversedString);
+  return reversedString;
+}
+
+reverseStringCase(
+  "L'âme nE se dÉvelopPe pAs sAnS ChAngEmenT, eT leS DoUtes Sont EsSentIels à la crOisSanCe."
+);
+
+reverseStringCases(
+  "L'âme nE se dÉvelopPe pAs sAnS ChAngEmenT, eT leS DoUtes Sont EsSentIels à la crOisSanCe."
+);
--- stringFromNumber.js
+++ stringFromNumber.js
@@ -1,34 +1,26 @@
-function reverseStringCase(text) {
-  let reversedString = [];
+const numbers = [
+  ["0", "zero"],
+  ["1", "one"],
+  ["2", "two"],
+  ["3", "three"],
+  ["4", "four"],
+  ["5", "five"],
+];
 
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] == text[i].toUpperCase()) {
-      reversedString.push(text[i].toLowerCase());
-    } else {
-      reversedString.push(text[i].toUpperCase());
-    }
+const stringFromNumber = (num) => {
+  if (num >= 0 && num <= 5) {
+    const getIndex = numbers[num];
+    return console.log(getIndex[1]);
   }
 
-  console.log(reversedString.join(""));
-  return reversedString.join("");
-}
+  return console.log(`The number '${num}' is out of range`);
+};
 
-function reverseStringCases(text) {
-  const reversedString = text
-    .split("")
-    .map((item) =>
-      item === item.toUpperCase() ? item.toLowerCase() : item.toUpperCase()
-    )
-    .join("");
-
-  console.log(reversedString);
-  return reversedString;
-}
-
-reverseStringCase(
-  "L'âme nE se dÉvelopPe pAs sAnS ChAngEmenT, eT leS DoUtes Sont EsSentIels à la crOisSanCe."
-);
-
-reverseStringCases(
-  "L'âme nE se dÉvelopPe pAs sAnS ChAngEmenT, eT leS DoUtes Sont EsSentIels à la crOisSanCe."
-);
+stringFromNumber(0);
+stringFromNumber(1);
+stringFromNumber(2);
+stringFromNumber(3);
+stringFromNumber(4);
+stringFromNumber(5);
+stringFromNumber(-1);
+stringFromNumber(6);
